1f53dab68cd3d5e505956cc69d0aefb2d633d117
Amra <ao2282@student.uni-lj.si>
2018-01-11 22:36:08 +0100

    changed title

--- html5/index.php
+++ html5/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE HTML>
 <!--
-	Phantom by HTML5 UP
+	Connect tones!
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 <html>
 	<head>
-		<title>Phantom by HTML5 UP</title>
+		<title>Connect tones!</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
